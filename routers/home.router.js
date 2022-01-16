92d82a44addb19a6641b9223f200253b01f974ea
Adam <adamrleigh96@gmail.com>
2022-01-16 15:53:32 +0000

    fix path

--- routers/home.router.js
+++ routers/home.router.js
@@ -1,7 +1,8 @@
 const homeRouter = require("express").Router();
+const path = require("path");
 
 homeRouter.route("/").get((req, res, next) => {
-  res.sendFile("../home.html");
+  res.sendFile(path.join(__dirname, "../home.html"));
 });
 
 module.exports = homeRouter;
