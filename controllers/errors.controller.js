cdb949147e945ce506e1501c395dcc1fc9e9dbe8
Adam <adamrleigh96@gmail.com>
2022-02-15 23:42:59 +0000

    amend returning error to throwing error

--- controllers/errors.controller.js
+++ controllers/errors.controller.js
@@ -1,5 +1,8 @@
 exports.validateQueries = async (req) => {
-    if (req.query.limit && /\D/.test(req.query.limit)) return { message: 'limit must be a number' };
-    if (req.query.p && /\D/.test(req.query.p)) return { message: 'p must be a number' };
-    if (req.query.order && !['asc', 'desc'].includes(req.query.order)) return { message: "order must be either 'asc' or 'desc'" };
-}
+  if (req.query.limit && /\D/.test(req.query.limit))
+    throw { status: 400, message: "limit must be a number" };
+  if (req.query.p && /\D/.test(req.query.p))
+    throw { status: 400, message: "p must be a number" };
+  if (req.query.order && !["asc", "desc"].includes(req.query.order))
+    throw { status: 400, message: "order must be either 'asc' or 'desc'" };
+};
