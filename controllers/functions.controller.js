db2d54d7aa021547518f53b08c5af4d7fe4c653c
Adam <adamrleigh96@gmail.com>
2022-02-15 23:43:28 +0000

    slight refactor of doThis

--- controllers/functions.controller.js
+++ controllers/functions.controller.js
@@ -1,33 +1,24 @@
-const db = require('../db/connection');
-const { validateQueries } = require('./errors.controller');
+const { validateQueries } = require("./errors.controller");
 
 const doThis = async (req, res, next, fun, code, key = null) => {
-    if (await validateQueries(req)) return res.status(400).send(await validateQueries(req))
-    fun(req)
-        .then((result) => {
-            if (key === null) res.status(code).send();
-            else {
-                const response = {};
-                response[key] = result;
-                res.status(code).send(response);
-            }
-        })
-        .catch(next)
+  try {
+    await validateQueries(req);
+    const result = await fun(req);
+    key === null
+      ? res.status(code).send()
+      : res.status(code).send({ [key]: result });
+  } catch (err) {
+    next(err);
+  }
 };
 
-
 exports.getThis = (req, res, next, fun, key) =>
-    doThis(req, res, next, fun, 200, key);
+  doThis(req, res, next, fun, 200, key);
 
 exports.patchThis = (req, res, next, fun, key) =>
-    doThis(req, res, next, fun, 200, key);
-
+  doThis(req, res, next, fun, 200, key);
 
 exports.postThis = (req, res, next, fun, key) =>
-    doThis(req, res, next, fun, 201, key);
+  doThis(req, res, next, fun, 201, key);
 
-
-exports.deleteThis = (req, res, next, fun) =>
-    doThis(req, res, next, fun, 204);
-
-
+exports.deleteThis = (req, res, next, fun) => doThis(req, res, next, fun, 204);
